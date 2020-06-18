540fb6c5977939a590f6b5bdea876454f06807f9
Darrell-Conklin <dmconklin84@gmail.com>
2020-06-18 16:37:35 -0500

    Endpoints for all CRUD actions

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-
-const db = require("../data/dbConfig.js");
+const accountsRouter = require('../routers/accounts-router')
 
 const server = express();
 
 server.use(express.json());
+server.use(accountsRouter)
 
 module.exports = server;
---
+++ routers/accounts-router.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../data/dbConfig')
+
+router.get('/', async (req,res,next) => {
+    try {
+        const message = await db.select("*").from("accounts")
+        res.json(message)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id', validateId(), async (req,res,next) => {
+    try {
+        res.json(req.account)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', validateBody(), async (req,res,next) => {
+    try {
+        const messageID = await db.insert(req.body).into("accounts")
+        const message = await db.select("*").from("accounts").where("id", messageID)
+        res.json(message)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.put('/:id', validateId(), validateBody(), async (req,res,next) => {
+    try {
+        const payload = {
+            name: req.body.name,
+            budget: req.body.budget
+        }
+        await db("accounts").update(payload).where("id", req.params.id)
+        const message = await db.first("*").from("accounts").where("id", req.params.id)
+        res.json(message)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.delete("/:id", validateId(), async (req,res,next) => {
+    try {
+        const success = await db("accounts").where("id", req.params.id).del()
+        if (!success) {
+            res.json({
+                message: "Could not delete the following account",
+                ...req.account
+            })
+        } else if (success) {
+            res.json({
+                message: "The following account was deleted",
+                ...req.account
+            })
+        }
+    } catch(err) {
+        next(err)
+    }
+})
+
+function validateId() {
+    return (req,res,next) => {
+        db.select("*").from("accounts").where("id", req.params.id)
+            .then(account => {
+                if (account.length < 1) { 
+                    return res.status(404).json({
+                        message: "The account does not exist"
+                    })
+                }
+                [req.account] = account
+                next()
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(500).json({
+                    message: "There was an error retrieving the account"
+                })
+            })
+    }
+}
+
+function validateBody() {
+    return (req,res,next) => {
+        if (!req.body.name || !req.body.budget) {
+            res.status(400).json({
+                message: "Name and Budget required"
+            })
+        }
+        next()
+    }
+}
+
+
+
+module.exports = router
